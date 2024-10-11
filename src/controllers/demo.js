2b877a41fe13f454c5440873b1046c12e33ac57d
Arup Maity <arupmaity079@gmail.com>
2024-10-11 14:03:40 +0530

    added demo router

--- src/controllers/demo.js
+++ src/controllers/demo.js
@@ -1,21 +1,22 @@
 import { Router } from 'express'
 import prisma from '../config/prisma.js';
+import { UsersData } from '../data/user.js';
 const demoRouter = Router();
 
 demoRouter.post('/create-employee', async (req, res) => {
    try {
-      const body = req.body
-      // Validate email
-      const checkEmail = await prisma.users.findUnique({
-         where: { email: body.email },
-      })
-      if (checkEmail) {
-         return res.status(409).json({ success: false, message: 'Email already exists' })
-      }
-      // Add logic to create a new employee
-      const employee = await prisma.users.create({
-         data: body
-      })
+
+      UsersData.forEach(async (user) => {
+         const checkEmail = await prisma.users.findUnique({
+            where: { email: user.email },
+         })
+         if (!checkEmail) {
+            await prisma.users.create({
+               data: user
+            })
+         }
+      });
+
       return res.status(200).json({ success: true, message: 'Employee create successfully' })
    } catch (error) {
       console.log(error)
