29d62b81bbc44bf418568c424df5a63da6592f6f
Arup Maity <arupmaity079@gmail.com>
2024-10-15 18:21:30 +0530

    complete leave format CURD

--- src/controllers/leaveFormat.js
+++ src/controllers/leaveFormat.js
@@ -7,20 +7,29 @@ leaveFormatRouter.use(adminAuthentication())
 
 leaveFormatRouter.post('/create-format', async (req, res) => {
    try {
-      const body = req.body
-      const checkName = await prisma.leaveFormat.findUnique({
-         where: { name: body.name }
+      const { name, description = '', type } = req.body
+      console.log(req.body)
+
+      const checkFormat = await prisma.leaveFormat.findUnique({
+         where: { name: name }
       })
-      if (checkName) {
+      if (checkFormat) {
          return res.status(409).json({ success: false, message: 'Format name already exists' })
       }
-      const format = await prisma.leaveFormat.create({
-         data: body
+      const createFormat = await prisma.leaveFormat.create({
+         data: {
+            name,
+            description,
+            leaveRelationship: {
+               create: type.map((item) => ({
+                  type: { connect: { id: item?.typeId } },
+                  value: item.value
+               })),
+            }
+         }
       })
-      if (!format) {
-         return res.status(400).json({ success: false, message: 'Error creating format' })
-      }
-      return res.status(201).json({ success: true, message: 'Format created successfully', format })
+      if (!createFormat) return res.status(409).json({ success: false, message: 'Error creating format' })
+      return res.status(200).json({ success: true, message: 'Format created successfully' })
    } catch (error) {
       console.log(error)
       return res.status(500).json({ success: false, message: 'Error creating format', error })
@@ -28,20 +37,58 @@ leaveFormatRouter.post('/create-format', async (req, res) => {
 })
 leaveFormatRouter.put('/update-format/:id', async (req, res) => {
    try {
-      const body = req.body
-      const checkName = await prisma.leaveFormat.findUnique({
+      const leaveFormatId = parseInt(req.params.id)
+      const { name, description, type } = req.body
+      const checkFormat = await prisma.leaveFormat.findUnique({
          where: {
-            name: body.name,
-            NOT: { id: parseInt(req.params.id) }
+            name: name,
+            NOT: { id: leaveFormatId }
          }
       })
-      if (checkName) {
+      if (checkFormat) {
          return res.status(409).json({ success: false, message: 'Format name already exists' })
       }
       const updatedFormat = await prisma.leaveFormat.update({
-         where: { id: parseInt(req.params.id) },
-         data: body
-      })
+         where: { id: leaveFormatId },
+         data: {
+            name,
+            description,
+            leaveRelationship: {
+               upsert: type.map((item) => ({
+                  where: {
+                     formatId_typeId: {
+                        formatId: leaveFormatId,
+                        typeId: item.typeId,
+                     }
+                  },
+                  create: {
+                     type: { connect: { id: item.typeId } },
+                     value: item.value,
+                  },
+                  update: {
+                     value: item.value,
+                  },
+               })),
+            },
+         },
+      });
+      if (!updatedFormat) return res.status(409).json({ success: false, message: 'Format not updated' })
+
+      // Get an array of typeIds from the provided type array
+      const typeIds = type?.map((item) => item?.typeId);
+      // Delete LeaveRelationship entries not associated with the provided formatId and typeIds
+      const deleteFormatRelation = await prisma.leaveRelationship.deleteMany({
+         where: {
+            formatId: leaveFormatId, // Match the specified formatId
+            NOT: {
+               typeId: {
+                  in: typeIds, // Keep only the relationships with these typeIds
+               },
+            },
+         },
+      });
+
+
       return res.status(200).json({ success: true, message: 'Format updated successfully', updatedFormat })
    } catch (error) {
       console.log(error)
@@ -51,7 +98,14 @@ leaveFormatRouter.put('/update-format/:id', async (req, res) => {
 leaveFormatRouter.get('/read-format/:id', async (req, res) => {
    try {
       const format = await prisma.leaveFormat.findUnique({
-         where: { id: parseInt(req.params.id) }
+         where: { id: parseInt(req.params.id) },
+         include: {
+            leaveRelationship: {
+               include: {
+                  type: true
+               }
+            }
+         }
       })
       if (!format) {
          return res.status(409).json({ success: false, message: 'Format not found' })
@@ -64,8 +118,31 @@ leaveFormatRouter.get('/read-format/:id', async (req, res) => {
 })
 leaveFormatRouter.get('/all-formats', async (req, res) => {
    try {
-      const formats = await prisma.leaveFormat.findMany()
-      return res.status(200).json({ success: true, message: 'All formats retrieved successfully', formats })
+      const { search, column = 'createdAt', sortOrder = 'desc', page = 1, limit = 15 } = req.query
+      const conditions = {}
+      if (search) {
+         conditions.name = { contains: search, mode: "insensitive" }
+      }
+      const query = {};
+      if (column && sortOrder) {
+         query.orderBy = { [column]: sortOrder }
+      }
+      const formats = await prisma.leaveFormat.findMany({
+         where: conditions,
+         include: {
+            leaveRelationship: {
+               include: {
+                  type: true
+               }
+            }
+         },
+         take: +limit,
+         skip: (+page - 1) * +limit,
+         ...query
+      })
+      const total = await prisma.leaveType.count({ where: conditions })
+
+      return res.status(200).json({ success: true, message: 'All formats retrieved successfully', formats, total })
    } catch (error) {
       console.log(error)
       return res.status(500).json({ success: false, message: 'Error retrieving formats', error })
